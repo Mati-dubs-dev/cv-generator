003a91f648e9a8f36fcae5cdd15ae92eb68d394d
Mati Dubs <Mati.dubs.dev@gmail.com>
2026-04-24 03:58:43 -0300

    agrego vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/react'
 import AnalyticsTracker from './analytics-tracker'
 
 export const metadata = {
@@ -13,8 +14,14 @@ export default function RootLayout({ children }) {
       <body className="font-body antialiased bg-white text-navy-900">
         {children}
 
+        {/* Tu tracker personalizado */}
         <AnalyticsTracker />
+
+        {/* Google Analytics */}
         <GoogleAnalytics gaId="G-TZ8VCXDHY3" />
+
+        {/* Vercel Analytics */}
+        <Analytics />
       </body>
     </html>
   )
